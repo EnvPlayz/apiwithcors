832391b829d13f19d85474fafd2e1544e3c26254
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2021-09-13 22:51:53 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const { default: axios } = require("axios");
+const { json } = require("express");
+const express = require("express");
+const app = express();
+app.get("/", (req,res) => {
+    res.send("server has nothing to do :D please enter something after this url for the server to start giving good responses")
+});
+app.get("/http/:url", (req,res) => {
+    var url = req.params.url;
+        axios.get(`http://${url}`).then(function (data) {
+            if(data.status==200){
+                res.send(data.data)
+            }
+            });
+});
+app.get("/https/:url", (req,res) => {
+    var url = req.params.url;
+    axios.get(`https://${url}`).then(function (data) {
+        if(data.status==200){
+            res.send(data.data)
+        }
+    });
+});
+app.listen(process.env.PORT || 3000);
