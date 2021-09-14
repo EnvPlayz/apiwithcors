bf1aca710124c64d72e8fb07248d6b6f9099ec85
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2021-09-14 20:08:25 +0300

    he

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get("/http/:url", (req,res) => {
             if(data.status==200){
                 res.send(data.data)
             }
+            }).catch(err => {
+                // res.send(err)
+                res.send(`<center><hr>The request had an ${err.name} . try again and check spelling.${err.message}<hr>We tried to sent a GET request to ${err.config.url} <center>`)
             });
 });
 app.get("/https/:url", (req,res) => {
@@ -19,11 +22,29 @@ app.get("/https/:url", (req,res) => {
         if(data.status==200){
             res.send(data.data)
         }
+    }).catch(err => {
+        // res.send(err)
+        res.send(`<center><hr>The request had an ${err.name} . try again and check spelling.${err.message}<hr>We tried to sent a GET request to ${err.config.url}<center>`)
     });
 });
-app.get("/gamejoin/",(req,res) =>{
-    axios.get("https://www.roblox.com/games/getgameinstancesjson?placeId=286090429&startindex=0").then(function (data) {
-            console.log(data.data)
-    })
-})
+app.get("/getfromapi/robloxname/:url", (req,res) => {
+    var url = req.params.url;
+    axios.get(`https://api.roblox.com/users/get-by-username?username=${url}`).then(function (data) {
+        res.send(data.data)
+    }).catch(err => {
+        // res.send(err)
+        res.send(`<center><hr>The request had an ${err.name} . try again and check spelling.${err.message}<hr>We tried to sent a GET request to ${err.config.url}<center>`)
+    });
+});
+app.get("/getfromapi/robloxavatarhead/:url", (req,res) => {
+    var url = req.params.url;
+    axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?format=Png&isCircular=true&size=48x48&userIds=${url}`).then(function (data) {
+        if(data.status==200){
+            res.send({return:data.data})
+        }
+    }).catch(err => {
+        // res.send(err)
+        res.send(`<center><hr>The request had an ${err.name} . try again and check spelling.${err.message}<hr>We tried to sent a GET request to ${err.config.url}<center>`)
+    });
+});
 app.listen(process.env.PORT || 5000);
